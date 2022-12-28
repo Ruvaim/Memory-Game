4dfea326366cf0945a2ac14378293476ba64c2f4
Ruvaim <ruvaim51@gmail.com>
2022-12-28 21:17:41 +0530

    Countdown timer added and img front changed

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const cards = document.querySelectorAll(".card");
-let playerLivesCounts=document.querySelector(".span");
-let playerLives = 6;
-
-playerLivesCounts.innerHTML=playerLives;
-// console.log(cards);
+const cards = document.querySelectorAll('.card');
+// let playerLivesCounts=document.querySelector(".span");
+let counter = document.querySelector('.span');
+// let playerLives = 6;
+let totalTime = 60;
 
 // variable
 
@@ -11,7 +10,7 @@ var isFlipped = false;
 var firstCard;
 var secondCard;
 
-cards.forEach((card) => card.addEventListener("click", flip));
+cards.forEach((card) => card.addEventListener('click', flip));
 
 // var flip = () => {
 //   this.classList.add("flip");
@@ -28,105 +27,121 @@ cards.forEach((card) => card.addEventListener("click", flip));
 //   }
 // }
 
-function flip(){
-  // console.log("card flipped");
-  // console.log(this);
-  // let flipCards = document.querySelectorAll(".flip");
-  this.classList.add("flip");
+function flip() {
+  this.classList.add('flip');
 
-  // if(flipCards.length === 2){
-  //   console.log("check",flipCards);
-  // }
-
-  // if(flipcards.classList ===flip ){
-  //   flipcards.style.pointerEvents ="none";
-  // }
   if (!isFlipped) {
     isFlipped = true;
     firstCard = this;
-  }else{
+  } else {
     secondCard = this;
-    if(firstCard===secondCard){
-      // console.log("same");
-      secondCard=null;
+    if (firstCard === secondCard) {
+      secondCard = null;
     }
-    // console.log(secondCard);
-    // console.log(firstCard);
-    // console.log(secondCard);
-
     checkIt();
   }
 }
 
 var checkIt = () => {
-  firstCard.dataset.image === secondCard.dataset.image ? success():fail()
-}
+  firstCard.dataset.image === secondCard.dataset.image ? success() : fail();
+};
 
-let flipped=0;
+let flipped = 0;
 var success = () => {
-  if(firstCard.classList== "card flip" && secondCard.classList == "card flip"){
-    flipped=flipped+2;
-    console.log(flipped);
+  if (
+    firstCard.classList == 'card flip' &&
+    secondCard.classList == 'card flip'
+  ) {
+    flipped = flipped + 2;
+    // console.log(flipped);
   }
-  if(flipped===20){
-    restart("You WON");
+  if (flipped === 20) {
+    restart('You WON');
   }
-  // console.log(flipped);
-  firstCard.removeEventListener("click", flip);
-  secondCard.removeEventListener("click", flip);
+  firstCard.removeEventListener('click', flip);
+  secondCard.removeEventListener('click', flip);
   reset();
-}
+};
 
 var fail = () => {
   setTimeout(() => {
-    firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
     reset();
   }, 500);
-  playerLives--;
+  // playerLives--;
   // console.log(playerLives);
-  playerLivesCounts.innerHTML=playerLives;
-  if(playerLives===0){
-    restart("You Lose,Try again");
+  // playerLivesCounts.innerHTML = playerLives;
+  if (count === 0) {
+    restart('Time up, Try Again');
   }
-
-}
-var restart=(text) => {
-
+};
+var restart = (text) => {
   cards.forEach((c) => {
-    c.classList.remove("flip");
-    c.addEventListener("click", flip);
-  })
+    c.classList.remove('flip');
+    c.addEventListener('click', flip);
+  });
   isFlipped = false;
   firstCard = null;
   secondCard = null;
   secondCard = null;
-  // console.log(cards);
+  shuffle1(numbers);
   shuffle();
-  shuffle();
-  playerLives=6;
-  playerLivesCounts.innerHTML=playerLives;
-  setTimeout(() => window.alert(text),100);
-}
+  count = 50;
+  setTimeout(() => window.alert(text), 100);
+};
 
 var reset = () => {
   isFlipped = false;
   firstCard = null;
   secondCard = null;
   secondCard = null;
-}
+};
 
 // shuffle
+var numbers = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+];
 
-var shuffle = () => {
-  cards.forEach((card) => {
-    var index = Math.floor(Math.random() * 20);
-    // console.log(index);
-    card.style.order = index;
-    // console.log("card",card.style.order);
-  });
+function shuffle1(o) {
+  for (
+    var j, x, i = o.length;
+    i;
+    j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+  );
+  return o;
 }
 
-window.addEventListener("load", shuffle())
+var random1 = shuffle1(numbers);
+
+var shuffle = () => {
+  let i = 0;
+  cards.forEach((card) => {
+    // var index = Math.floor(Math.random() * 20);
+    // var index = random1;
+    card.style.order = random1[i];
+    if (i < 19) {
+      i++;
+    }
+  });
+};
+count = 50;
+
+setInterval(() => {
+  if (count > 0) {
+    count--;
+    if (count < 10) {
+      counter.style.color = 'red';
+    }
+    counter.innerHTML = count;
+  }
+  if (count === 0) {
+    counter.style.color = 'white';
+
+    fail();
+  }
+}, 1000);
+
+window.addEventListener('load', shuffle());
 
 // var score = document.querySelector(".score");
